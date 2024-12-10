ca3fb8010f3cae95a964fccd5fe0501d9c557ba0
1GodLess5 <gacekalbert@seznam.cz>
2024-12-10 18:04:03 +0100

    Added Header and Footer Components

---
+++ src/components/FooterComponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const FooterComponent = () => {
+  return (
+    <div>
+        <footer className='footer'>
+            <span className='footerSpan'>Copyright © 2024 cz.godless</span>
+        </footer>
+    </div>
+  )
+}
+
+export default FooterComponent
