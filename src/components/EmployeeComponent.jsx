9e64f456aa37309419bbd816e4dda927e7975c35
1GodLess5 <gacekalbert@seznam.cz>
2024-12-11 14:06:10 +0100

    + Added Update button to ListEmployeeComponent, ~ Changed behaviour of EmployeeComponent -> now dynamically displays h2 (Add Employee / Update Employee)

--- src/components/EmployeeComponent.jsx
+++ src/components/EmployeeComponent.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { createEmployee } from '../services/EmployeeService'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 
 const EmployeeComponent = () => {
     const [firstName, setFirstName] = useState('');
@@ -14,6 +14,7 @@ const EmployeeComponent = () => {
     });
 
     const navigator = useNavigate();
+    const {id} = useParams();
 
     function saveEmployee(e) {
         e.preventDefault();
@@ -27,6 +28,14 @@ const EmployeeComponent = () => {
                 navigator('/employees');
             });
         }         
+    }
+
+    function pageTitle() {
+        if(id) {
+            return <h2 className='text-center'>Update Employee</h2>
+        } else {
+            return <h2 className='text-center'>Add Employee</h2>
+        }
     }
 
     function validateForm() {
@@ -63,7 +72,9 @@ const EmployeeComponent = () => {
     <div className='container mt-5'>
         <div className='row'>
             <div className='card col-md-6 offset-md-3'>
-                <h2 className='text-center'>Add Employee</h2>
+                {
+                    pageTitle()
+                }
                 <div className='card-body'>
                     <form onSubmit={saveEmployee}>
                         <div className='form-group mb-2'>
